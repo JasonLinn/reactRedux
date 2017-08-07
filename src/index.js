1ff26e4f45a01c13d1875b2bd498c690b0a4620b
JasonLinn <cheng11220@gmail.com>
2017-08-07 10:52:24 +0800

    change index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-//Create a new component. This component should produce
-//some HTML
-const App = function (){
-    return <div>Hi!</div>
-}
-
-//Take this component's generated HTML and put it 
-//on the page (in the DOM)
-
-
-ReactDOM.render(<App/>);
-=======
 
 import SearchBar from './components/search_bar';
 
@@ -27,4 +14,3 @@ const App = (params) =>{
 }
 
 ReactDOM.render(<App/>,document.querySelector('.container'));
->>>>>>> a414747a4644ef22ca02ecbb9b81d0e9e1277ae5
