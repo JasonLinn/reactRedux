264bc1c78dd2ebc91c2f37dbe1ffdaf2b8810248
JasonLinn <cheng11220@gmail.com>
2017-08-08 14:14:26 +0800

    add bundle

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -32,6 +33,8 @@ class App extends Component{
         })
     }
     render(){
+        const videoSearch = _.debounce((term)=>{this.videoSearch(term)},300); //debounce 可以讓fn執行延遲_毫秒
+
         return( 
             <div>
                 {/* {this.state.videos} */}
